c55a04a26f00d27c6a92552d835e2f216ca35ae6
Canon-K41 <kai.kensuke.255@gmail.com>
2025-03-28 20:24:53 +0900

    [REMOVE] submodule

---
+++ Tutorial/omikuji.js
@@ -0,0 +1,26 @@
+let userName;
+let userResult;
+
+userName = prompt('お名前を教えて下さい。');
+if (userName == '') {
+    userName = '名無し';
+}
+document.getElementById('name').innerHTML = userName;
+
+let rand = Math.floor(Math.random() * 5);
+if (rand == 0) {
+    userResult = '大吉';
+}
+if (rand == 1) {
+    userResult = '中吉';
+}
+if (rand == 2) {
+    userResult = '小吉';
+}
+if (rand == 3) {
+    userResult = '吉';
+}
+if (rand == 4) {
+    userResult = '凶';
+}
+document.getElementById('result').innerHTML = userResult;
